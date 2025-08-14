3fdf8f6c5d525c5996212a38778d390a07abd8b3
bdaniels013 <bdaniels013@gmail.com>
2025-08-14 17:30:13 -0500

    Delete api/admin/logout.js

--- api/admin/logout.js
+++
@@ -1,7 +0,0 @@
-import { clearCookie } from '../../lib/cookies.js';
-import { COOKIE } from '../../lib/auth.js';
-
-export default async function handler(req, res) {
-  clearCookie(res, COOKIE);
-  res.status(200).json({ ok: true });
-}
